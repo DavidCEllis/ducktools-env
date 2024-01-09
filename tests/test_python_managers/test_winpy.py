df4e628f43107da6b3d19b60450702fbe1b6ceff
David C Ellis <ducksual@gmail.com>
2024-01-09 18:14:07 +0000

    Test windows python version detection

---
+++ tests/test_python_managers/test_winpy.py
@@ -0,0 +1,33 @@
+import pytest
+
+import sys
+import os.path
+
+from ducktools.envman.env_builder.shared import PythonVersion
+from ducktools.envman.env_builder.winpy_finder import get_py_install_versions
+
+known_versions = [
+    PythonVersion.from_str(
+        "3.10",
+        os.path.join(os.path.expandvars("%LOCALAPPDATA%"), "Programs", "Python", "Python310", "python.exe"),
+    ),
+    PythonVersion.from_str(
+        "3.11",
+        os.path.join(os.path.expandvars("%LOCALAPPDATA%"), "Programs", "Python", "Python311", "python.exe"),
+    ),
+    PythonVersion.from_str(
+        "3.12",
+        os.path.join(os.path.expandvars("%LOCALAPPDATA%"), "Programs", "Python", "Python312", "python.exe"),
+    ),
+]
+
+
+installed_python = [item for item in known_versions if os.path.exists(item.executable)]
+
+
+@pytest.mark.skipif(sys.platform != "win32", reason="Windows only test")
+def test_winpy_real():
+    versions = get_py_install_versions(precise=False)
+
+    for ver in installed_python:
+        assert ver in versions
