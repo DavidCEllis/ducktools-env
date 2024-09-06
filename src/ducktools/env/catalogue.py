ac1e8b4b0a70909e3aa054867378223a72aba4d6
David C Ellis <ducksual@gmail.com>
2024-09-06 15:12:20 +0100

    Delete the cache folder if the creation fails.

--- src/ducktools/env/catalogue.py
+++ src/ducktools/env/catalogue.py
@@ -382,6 +382,7 @@ class TempCatalogue(BaseCatalogue):
 
         new_cachename = f"env_{self.env_counter}"
         self.env_counter += 1
+
         cache_path = os.path.join(self.catalogue_folder, new_cachename)
 
         # Find a valid python executable
@@ -419,6 +420,8 @@ class TempCatalogue(BaseCatalogue):
                     [python_exe, "-m", "venv", "--without-pip", cache_path], check=True
                 )
         except _laz.subprocess.CalledProcessError as e:
+            # Try to delete the folder if it exists
+            _laz.shutil.rmtree(cache_path, ignore_errors=True)
             raise VenvBuildError(f"Failed to build venv: {e}")
 
         # Construct the TemporaryEnv to get the path to python.exe
@@ -494,6 +497,8 @@ class TempCatalogue(BaseCatalogue):
                         check=True,
                     )
                 except _laz.subprocess.CalledProcessError as e:
+                    # Try to delete the folder if it exists
+                    _laz.shutil.rmtree(cache_path, ignore_errors=True)
                     raise VenvBuildError(f"Failed to install dependencies: {e}")
 
             # Get pip-freeze list to use for installed modules
