9d6a0bf328c06d858b43a7c9c02ff02e0566ba00
David C Ellis <ducksual@gmail.com>
2024-10-23 14:06:00 +0100

    Add new registry function and significant main() function cleanup

--- src/ducktools/env/register.py
+++ src/ducktools/env/register.py
@@ -20,11 +20,85 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+import os.path
 
-from _sqlclasses import SQLClass
+from ducktools.classbuilder.prefab import Prefab
+
+from ._lazy_imports import laz as _laz
+from ._sqlclasses import SQLAttribute, SQLClass, SQLContext
+from ._logger import log
 
 
-class ScriptRegister(SQLClass):
-    name: str
+class RegisteredScript(SQLClass):
+    rowid: int = SQLAttribute(default=None, primary_key=True)
+    name: str = SQLAttribute(unique=True)
+    path: str = SQLAttribute(unique=True)
+
+    @classmethod
+    def from_script(cls, relpath):
+        name = os.path.splitext(os.path.basename(relpath))[0]
+        path = os.path.abspath(relpath)
+        return cls(name=name, path=path)
+
+
+class RegisterManager(Prefab, kw_only=True):
     path: str
 
+    @property
+    def connection(self) -> SQLContext:
+        if not os.path.exists(self.path):
+            os.makedirs(os.path.dirname(self.path), exist_ok=True)
+            with SQLContext(self.path) as con:
+                RegisteredScript.create_table(con)
+
+        return SQLContext(self.path)
+
+    def add_script(self, script_path: str) -> RegisteredScript:
+        if not os.path.exists(script_path):
+            raise FileNotFoundError(f"'{script_path}' not found")
+
+        new_row = RegisteredScript.from_script(script_path)
+
+        with self.connection as con:
+            try:
+                new_row.insert_row(con)
+            except _laz.sql.DatabaseError as e:
+                raise RuntimeError(
+                    f"Could not add script: '{script_path}' to the register. SQL Error: {e}"
+                )
+
+        return new_row
+
+    def retrieve_script(self, script_name: str) -> RegisteredScript:
+        with self.connection as con:
+            row_filter = {
+                "name": script_name,
+            }
+            row = RegisteredScript.select_row(con, row_filter)
+
+        if row is None:
+            raise RuntimeError(
+                f"'{script_name}' is not a registered script. "
+                f"Use `python -m ducktools-env list --scripts` to show registered scripts."
+            )
+
+        if not os.path.exists(row.path):
+            self.remove_script(script_name)
+
+            raise RuntimeError(
+                f"'{script_name}' file at '{row.path} does not exist, removed from registry."
+            )
+
+        return row
+
+    def remove_script(self, script_name: str) -> None:
+        row = self.retrieve_script(script_name)
+
+        with self.connection as con:
+            row.delete_row(con)
+
+    def list_registered_scripts(self) -> list[RegisteredScript]:
+        with self.connection as con:
+            rows = RegisteredScript.select_rows(con)
+
+        return rows
