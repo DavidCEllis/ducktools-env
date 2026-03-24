016fad785ec93b17b1818bbd59b509a34bd0cbe4
David C Ellis <ducksual@gmail.com>
2026-03-24 17:01:23 +0000

    remove tomli from lazy imports

--- src/ducktools/env/_lazy_imports.py
+++ src/ducktools/env/_lazy_imports.py
@@ -1,18 +1,18 @@
 # ducktools.env
 # MIT License
-# 
+#
 # Copyright (c) 2024 David C Ellis
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -20,7 +20,6 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-import sys
 
 from ducktools.lazyimporter import LazyImporter
 from ducktools.lazyimporter.capture import capture_imports
@@ -73,10 +72,7 @@ with capture_imports(laz):
     import warnings
     import zipfile
 
-    if sys.version_info >= (3, 11):
-        import tomllib as tomllib
-    else:
-        import tomli as tomllib
+    import tomllib
 
     from tempfile import TemporaryDirectory
     from urllib.request import urlopen
